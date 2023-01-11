4a3ded170575c3ed897f867087a0d79fc3349511
devolonter <devolonter@gmail.com>
2023-01-11 18:48:59 +0100

    Cleanup

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
-import { ColorModeScript } from "@chakra-ui/react"
-import * as React from "react"
-import * as ReactDOM from "react-dom/client"
-import { App } from "./App"
-import reportWebVitals from "./reportWebVitals"
-import * as serviceWorker from "./serviceWorker"
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client'
+import { App } from './App'
+import reportWebVitals from './reportWebVitals'
+import * as serviceWorker from './serviceWorker'
 
 
-const container = document.getElementById("root")
+const container = document.getElementById('root')
 if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <ColorModeScript />
     <App />
   </React.StrictMode>,
 )
