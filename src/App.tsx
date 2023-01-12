b6f2656faa87b658d320e82413a581bd2abb3984
devolonter <devolonter@gmail.com>
2023-01-12 11:30:05 +0100

    Completed cv header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,83 @@
 import {
-  ChakraProvider,
-  theme,
+  Badge,
+  Box,
+  ChakraProvider, Container, Divider, Flex, Grid, GridItem, HStack, Text,
+  theme, VStack,
 } from '@chakra-ui/react'
+import { AiFillLinkedin, AiOutlineGithub, MdEmail } from 'react-icons/all'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-
+    <Box
+      maxW="1280px"
+      mx="auto"
+      color={'gray.700'}
+    >
+      <Box
+        as={'header'}
+      >
+        <Grid
+          templateColumns="1fr auto"
+          gap={4}
+        >
+          <GridItem>
+            <Flex
+              alignItems={'start'}
+              direction={'column'}
+              h={'100%'}
+            >
+              <Text
+                fontSize={'1.5rem'}
+                textTransform={'uppercase'}
+                fontWeight={'bold'}
+              >
+                Artur Bikmullin
+              </Text>
+              <Text
+                color={'gray.600'}
+                mt={0}
+              >
+                Flutter developer, Full-Stack Web Developer and Game Development Practitioner
+              </Text>
+            </Flex>
+          </GridItem>
+          <GridItem>
+            <VStack
+              alignItems={'start'}
+              fontSize={'1.2rem'}
+            >
+              <HStack>
+                <MdEmail />
+                <Text>devolonter@gmail.com</Text>
+              </HStack>
+              <HStack>
+                <AiOutlineGithub />
+                <Text>github.com/devolonter</Text>
+              </HStack>
+              <HStack>
+                <AiFillLinkedin />
+                <Text>linkedin.com/in/devolonter</Text>
+              </HStack>
+            </VStack>
+            </GridItem>
+        </Grid>
+        <Box
+          py={2}
+          px={4}
+          bg={'gray.100'}
+          rounded={'xl'}
+          mt={4}
+        >
+          <Text>
+            I am an experienced web developer with over 15 years of experience in the field. My expertise lies in
+            front-end and back-end development, utilizing technologies such as HTML/CSS/JS, React, Vue, Node.js,
+            and WebGL to create engaging and interactive web experiences. I also have experience with game engines
+            such as Godot, and have a strong understanding of animation, shaders, and WebGL. I am constantly seeking
+            to improve my skills and stay current with the latest industry trends. I am excited about the opportunity
+            to bring my expertise to the role of a playable ads web developer.
+          </Text>
+        </Box>
+      </Box>
+    </Box>
   </ChakraProvider>
 )
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom/client'
 import { App } from './App'
 import reportWebVitals from './reportWebVitals'
 import * as serviceWorker from './serviceWorker'
+import './index.css'
 
 
 const container = document.getElementById('root')
